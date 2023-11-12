bd7c52d6ba818c23d2b336c5792ad617c9421970
johnmuthua <caveomuthama@gmail.com>
2023-11-12 17:15:50 +0300

    Add: created outline page

---
+++ src/pages/PageOne.tsx
@@ -0,0 +1,14 @@
+import Login from "../components/Login";
+import LoginTwo from "../components/LoginTwo";
+function App() {
+  return (
+    <>
+      <div className="flex space-between mt-[7%] flex-row">
+        <Login />
+        <LoginTwo />
+      </div>
+    </>
+  );
+}
+
+export default App;
