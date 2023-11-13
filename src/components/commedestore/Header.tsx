d0cafb43a6f20641e1fa6fc55a118bc7a1a1410c
johnmuthua <caveomuthama@gmail.com>
2023-11-13 16:55:14 +0300

    Add more componets to header

--- src/components/commedestore/Header.tsx
+++ src/components/commedestore/Header.tsx
@@ -1,8 +1,9 @@
+import { BsSearch } from "react-icons/bs";
 const Header = () => {
   return (
     <div className="flex flex-row justify-between items-center px-4 py-2 bg-gray-100 border-b-2 border-gray-200">
-      <div className="flex w-1/3 border-2 border-red-600"></div>
-      <div className="flex flex-row w-1/3 border-green-600 border-2 justify-center items-center">
+      <div className="flex w-1/3"></div>
+      <div className="flex flex-row w-1/3 justify-center items-center">
         <div className="border-4 text-center font-bold px-3 py-1 border-orange-400 rounded-[100%] text-lg text-blue-900">
           <p className="p-0 m-0">ML</p>
           <p className="p-0 -mt-2">LX</p>
@@ -12,13 +13,20 @@ const Header = () => {
         <div>
           <h4 className="text-blue-900 font-bold">My Account</h4>
         </div>
-        <div className="flex flex-row text-blue-900 font-bold">
+        <div className="flex flex-row text-blue-900 font-bold items-center">
           <span className="border-2 border-orange-400 rounded-[100%] px-2">
             <p>4</p>
           </span>
-          <h4>My Account</h4>
+          <h4 className="ml-2">Cart</h4>
         </div>
-        <div></div>
+        <div>
+          <input
+            type="text"
+            placeholder="Search for product"
+            className="relative py-2 px-4 border-2 border-blue-900"
+          />
+          <BsSearch className="absolute right-0 -mt-4 mr-4" />
+        </div>
       </div>
     </div>
   );
