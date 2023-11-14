f7c7bd248d44180ea3ac35e7bd1d1cebfd3cd188
johnmuthua <caveomuthama@gmail.com>
2023-11-14 12:53:46 +0300

    Fix: color corrections

--- src/components/commedestore/Hero.tsx
+++ src/components/commedestore/Hero.tsx
@@ -2,7 +2,7 @@ import HeroImg from "../../assets/hero.png";
 const Hero = () => {
   return (
     <div className="flex flex-col bg-gray-200 justify-center items-center md:flex-row">
-      <div className="w-2/3">
+      <div className="md:w-1/2">
         <div className="flex flex-col justify-center items-center">
           <h3 className="italic text-gray-400 md:text-2xl">
             The latest gear from
@@ -16,7 +16,7 @@ const Hero = () => {
           </button>
         </div>
       </div>
-      <div className="w-1/3">
+      <div className="md:w-1/2 flex justify-center items-center">
         <img src={HeroImg} alt="Hero image" className="" />
       </div>
     </div>
