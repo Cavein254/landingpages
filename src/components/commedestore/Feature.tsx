d4827b48f8b914de723c47dd9d464dfed95a16ed
johnmuthua <caveomuthama@gmail.com>
2023-11-14 10:43:57 +0300

    Add feature section

---
+++ src/components/commedestore/Feature.tsx
@@ -0,0 +1,19 @@
+const Feature = () => {
+  return (
+    <div className="flex flex-row py-8 px-[10%] justify-center items-center">
+      <div className="flex flex-col justify-center items-center">
+        <h4 className="uppercase font-bold text-lg py-2">
+          featured collections
+        </h4>
+        <p className="text-gray-400 text-center">
+          {" "}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at
+          massa eu tortor dignissim eleifend vitae quis nunc. Cras gravida
+          euismod justo. Praesent fermentum pellentesque arcu, id laoreet dui
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Feature;
--- src/components/commedestore/index.tsx
+++ src/components/commedestore/index.tsx
@@ -1,10 +1,12 @@
 import Header from "./Header";
 import Hero from "./Hero";
+import Feature from "./Feature";
 const Store = () => {
   return (
     <>
       <Header />
       <Hero />
+      <Feature />
     </>
   );
 };
