6706bd4de4c57da8f596f52d8e86a180d2ed0237
johnmuthua <caveomuthama@gmail.com>
2023-11-14 15:46:15 +0300

    Add popular products

--- src/context/ProductProvider.tsx
+++ src/context/ProductProvider.tsx
@@ -4,14 +4,12 @@ import { useState, useEffect } from "react";
 
 const ProductProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
-  console.log({ message: "initial" });
   useEffect(() => {
     fetch("https://fakestoreapi.com/products?limit=5")
       .then((res) => res.json())
       .then((data) => setProducts(data))
-      .catch((err) => console.log("an error occurred", { err }));
+      .catch((err) => console.error("an error occurred", { err }));
   }, []);
-  console.log({ mydata: products });
   return (
     <ProductContext.Provider value={products}>
       {children}
