9e88d70b3634a03b5f4a9d8694fda5b8502fe0ef
johnmuthua <caveomuthama@gmail.com>
2023-11-14 10:24:07 +0300

    Add hero section

--- src/components/commedestore/Hero.tsx
+++ src/components/commedestore/Hero.tsx
@@ -1,7 +1,24 @@
+import HeroImg from "../../assets/hero.png";
 const Hero = () => {
   return (
-    <div>
-      <h1>Testing another class</h1>
+    <div className="flex flex-col bg-gray-200 justify-center items-center md:flex-row">
+      <div className="w-2/3">
+        <div className="flex flex-col justify-center items-center">
+          <h3 className="italic text-gray-400 md:text-2xl">
+            The latest gear from
+          </h3>
+          <h1 className="text-2xl font-bold uppercase md:text-4xl">
+            comme de store
+          </h1>
+          <h3 className="text-sm">X</h3>
+          <button className="px-6 py-2 border-2 border-orange-300 uppercase font-bold">
+            view collection
+          </button>
+        </div>
+      </div>
+      <div className="w-1/3">
+        <img src={HeroImg} alt="Hero image" className="" />
+      </div>
     </div>
   );
 };
