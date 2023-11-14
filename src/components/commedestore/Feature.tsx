7f9103e308ef7ac288779147104c05ddc6e8024e
johnmuthua <caveomuthama@gmail.com>
2023-11-14 11:20:15 +0300

    Add featureitems

--- src/components/commedestore/Feature.tsx
+++ src/components/commedestore/Feature.tsx
@@ -1,18 +1,23 @@
+import FeaturedItems from "./FeatureItems";
 const Feature = () => {
   return (
-    <div className="flex flex-row py-8 px-[10%] justify-center items-center">
-      <div className="flex flex-col justify-center items-center">
-        <h4 className="uppercase font-bold text-lg py-2">
-          featured collections
-        </h4>
-        <p className="text-gray-400 text-center">
-          {" "}
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at
-          massa eu tortor dignissim eleifend vitae quis nunc. Cras gravida
-          euismod justo. Praesent fermentum pellentesque arcu, id laoreet dui
-        </p>
+    <>
+      <div className="flex flex-row py-8 px-[10%] justify-center items-center">
+        <div className="flex flex-col justify-center items-center">
+          <h4 className="uppercase font-bold text-lg py-2">
+            featured collections
+          </h4>
+          <p className="text-gray-400 text-center">
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at
+            massa eu tortor dignissim eleifend vitae quis nunc. Cras gravida
+            euismod justo. Praesent fermentum pellentesque arcu, id laoreet dui
+          </p>
+        </div>
+        <div></div>
       </div>
-    </div>
+      <FeaturedItems />
+    </>
   );
 };
 
---
+++ src/components/commedestore/FeatureItems.tsx
@@ -0,0 +1,16 @@
+const FeaturedItems = () => {
+  return (
+    <div className="px-[10%] py-[2%] flex flex-col">
+      <div className="flex flex-col">
+        <div>
+          <div></div>
+          <div>
+            <img src="https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedItems;
